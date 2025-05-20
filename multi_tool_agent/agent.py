591bb667b9f91c8aaa22bdb1570c61ddbe4ac751
Kafiling <panthawit.k@gmail.com>
2025-05-21 00:54:50 +0700

    backup

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -2,6 +2,8 @@ import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 
+from .sub_agents.detectIngredientAgent.agent import detectIngredientAgent
+
 def get_weather(city: str) -> dict:
     """Retrieves the current weather report for a specified city.
 
@@ -55,13 +57,14 @@ def get_current_time(city: str) -> dict:
 
 
 root_agent = Agent(
-    name="weather_time_agent",
+    name="manager",
     model="gemini-2.0-flash",
     description=(
-        "Agent to answer questions about the time and weather in a city."
+        "Agent to detect ingredients in a refrigerator"
     ),
     instruction=(
-        "You are a helpful agent who can answer user questions about the time and weather in a city."
+        """You are an manager agent. You have 1 sub-agent, which is a detectIngredientAgent."""
     ),
+    sub_agents=[detectIngredientAgent],
     tools=[get_weather, get_current_time],
 )
---
+++ multi_tool_agent/sub_agents/detectIngredientAgent/agent.py
@@ -0,0 +1,27 @@
+"""Short description of the module here."""
+
+from google.adk import Agent
+
+
+
+MODEL = "gemini-2.5-flash-preview-04-17"
+
+
+
+detectIngredientAgent = Agent(
+    model=MODEL,
+    name="detectIngredientAgent",
+    description=(
+        "This agent identifies food ingredients from images of opened refrigerators."
+    ),
+    instruction="""You are an AI assistant specialized in identifying food ingredients from images.
+    Your task is to analyze the provided image of an opened refrigerator.
+    Identify all recognizable edible food items visible in the image.
+    Return the list as a list of objects. Each object should have two keys:
+    1. \"item_name\": The common name of the food item (string) with related emoji infront of string.
+    2. \"confidence\": Your estimated confidence in this identification (float, 0.0 to 1.0). (If not directly possible, state \"N/A\").
+    Exclude non-food items. Only include items with a confidence of 0.6 or higher.
+    """,
+    output_key="detectIngredientAgent_output",
+    #tools=[google_search],
+)
